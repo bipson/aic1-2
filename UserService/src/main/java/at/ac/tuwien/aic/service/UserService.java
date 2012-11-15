89ab563e7b09d9d034f96820b3561eba4051b56d
Philipp Raich <philipp.raich@gmail.com>
2012-11-15 17:14:24 +0100

    integrated geronimo deployment

--- UserService/src/main/java/at/ac/tuwien/aic/service/UserService.java
+++ UserService/src/main/java/at/ac/tuwien/aic/service/UserService.java
@@ -3,8 +3,11 @@ package at.ac.tuwien.aic.service;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
 
 @WebService
+@SOAPBinding(style = Style.RPC)
 public class UserService implements IUserService {
 
 	@Override
