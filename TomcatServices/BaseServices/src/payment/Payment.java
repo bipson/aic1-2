bed29bad1bbba39f30fbee37ceaf5c7f2730d74b
Philip Pickering <pgpickering@gmail.com>
2013-01-09 11:27:28 +0100

    payment logging uses log4j

--- TomcatServices/BaseServices/src/payment/Payment.java
+++ TomcatServices/BaseServices/src/payment/Payment.java
@@ -2,22 +2,26 @@ package payment;
 
 import javax.jws.WebService;
 
-@WebService(targetNamespace = "http://aic.service.payment/",
-            endpointInterface = "payment.IPayment", 
-            portName = "Payment", 
-            serviceName = "PaymentService")
+import org.apache.log4j.Logger;
+
+@WebService(targetNamespace = "http://aic.service.payment/", endpointInterface = "payment.IPayment", portName = "Payment", serviceName = "PaymentService")
 public final class Payment implements IPayment {
 	// Set to true when using mockup payment service.
 	private static final boolean MOCKUP = false;
-	
-    @Override
-    public Boolean pay(String ourAccountData, String userAccountData, Double amount) {
-    	if(MOCKUP) {
-    		System.out.println("Payment.pay: ourAccountData=\"" + ourAccountData + "\" userAccountData=\"" + userAccountData + "\" amount=\"" + amount + "\"");
-            return userAccountData.equals("{User Account Information}");
-    	} else {
-    		// Final service code.
-    		return true;
-    	}
-    }
+	private static Logger logger = Logger.getLogger(Payment.class
+			.getSimpleName());
+
+	@Override
+	public Boolean pay(String ourAccountData, String userAccountData,
+			Double amount) {
+		if (MOCKUP) {
+			logger.debug("Payment.pay: ourAccountData=\"" + ourAccountData
+					+ "\" userAccountData=\"" + userAccountData
+					+ "\" amount=\"" + amount + "\"");
+			return userAccountData.equals("{User Account Information}");
+		} else {
+			// Final service code.
+			return true;
+		}
+	}
 }
