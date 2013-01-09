bcece81422c1b27e940c0dcf227305fea2a12976
Juraj <jurajcik@gmail.com>
2013-01-09 12:52:01 +0100

    added logging for accounting. Changed format of logger.

--- TomcatServices/BaseServices/src/accounting/Accounting.java
+++ TomcatServices/BaseServices/src/accounting/Accounting.java
@@ -7,25 +7,30 @@ import javax.persistence.Persistence;
 
 import model.CompanyEntity;
 
-@WebService(targetNamespace = "http://aic.service.accounting/",
-            endpointInterface = "accounting.IAccounting",
-            portName = "Accounting",
-            serviceName = "AccountingService")
+import org.apache.log4j.Logger;
+
+@WebService(targetNamespace = "http://aic.service.accounting/", endpointInterface = "accounting.IAccounting", portName = "Accounting", serviceName = "AccountingService")
 public final class Accounting implements IAccounting {
 	// Set to true when using mockup accounting service.
 	private static final boolean MOCKUP = false;
-	
-	private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("aic.sentiment");
+
+	public Logger log = Logger.getLogger(Accounting.class);
+
+	private static final EntityManagerFactory emf = Persistence
+			.createEntityManagerFactory("aic.sentiment");
 
 	@Override
 	public Double getBill(String companyName) {
-		if(MOCKUP) {
-			System.out.println("Accounting.getBill: companyName=\"" + companyName + "\"");
-			if(companyName.equals("company1")) {
+		if (MOCKUP) {
+			log.debug("[MOCKUP] getBill() - companyName= \"" + companyName
+					+ "\"");
+			if (companyName.equals("company1")) {
 				return 34.5;
-			} else if(companyName.equals("company2") || companyName.equals("company5") || companyName.equals("company6")) {
+			} else if (companyName.equals("company2")
+					|| companyName.equals("company5")
+					|| companyName.equals("company6")) {
 				return 0.0;
-			} else if(companyName.equals("company3")) {
+			} else if (companyName.equals("company3")) {
 				return 1.0;
 			} else {
 				return Double.NaN;
@@ -33,71 +38,85 @@ public final class Accounting implements IAccounting {
 		} else {
 
 			EntityManager manager = emf.createEntityManager();
-			
+
 			try {
-				CompanyEntity company = manager.find(CompanyEntity.class, companyName);
-				
-				if(company != null) {
-					System.out.println("CompanyName: "+company.getCompanyName()+" CurrentBill: "+company.getCurrentBill());
+				CompanyEntity company = manager.find(CompanyEntity.class,
+						companyName);
+
+				if (company != null) {
+					log.debug("getBill() - CompanyName: "
+							+ company.getCompanyName() + " CurrentBill: "
+							+ company.getCurrentBill());
 					return company.getCurrentBill();
-				}
-				else
+				} else {
+					log.debug("getBill() - CompanyName: "
+							+ company.getCompanyName() + "- No bill found. ");
 					return Double.NaN;
-				
-			} finally {	
+				}
+
+			} finally {
 				manager.close();
 			}
 		}
-		
+
 	}
 
 	@Override
 	public Boolean charge(String companyName, Double amount) {
-		if(MOCKUP) {
-			System.out.println("Accounting.charge: companyName=\"" + companyName + "\" amount=\"" + amount + "\"");
-			return companyName.equals("company1") || companyName.equals("company2") || companyName.equals("company6");
+		if (MOCKUP) {
+			log.debug("[MOCKUP] charge() - companyName=\"" + companyName
+					+ "\" amount=\"" + amount + "\"");
+			return companyName.equals("company1")
+					|| companyName.equals("company2")
+					|| companyName.equals("company6");
 		} else {
 
 			EntityManager manager = emf.createEntityManager();
-			
+
 			try {
-				
+
 				manager.getTransaction().begin();
-				
-				CompanyEntity company = manager.find(CompanyEntity.class, companyName);
-				if(company == null){
+
+				CompanyEntity company = manager.find(CompanyEntity.class,
+						companyName);
+				if (company == null) {
 					manager.getTransaction().rollback();
 					return false;
 				}
-				
+
 				Double newBill = company.getCurrentBill() + amount;
-				if(newBill >= 0){
+				if (newBill >= 0) {
 					company.setCurrentBill(newBill);
 					manager.merge(company);
 					manager.getTransaction().commit();
-					
-					System.out.println("CompanyName: "+company.getCompanyName()+" NewBill: "+ newBill);
-					
+
+					log.debug(" charge() - CompanyName: "
+							+ company.getCompanyName() + " NewBill: " + newBill);
+
 					return true;
-				}else{
+				} else {
+					log.debug(" charge() - CompanyName: "
+							+ company.getCompanyName()
+							+ " The bill value can not be under 0. New value would be "
+							+ newBill);
 					manager.getTransaction().rollback();
 					return false;
 				}
-				
+
 			} finally {
 				manager.close();
 			}
-			
+
 		}
 	}
 
 	@Override
 	public String getOurAccountData() {
-		if(MOCKUP) {
-			System.out.println("Accounting.getOurAccountData");
+		if (MOCKUP) {
+			log.debug("[MOCKUP] getOurAccountData() ");
 			return "{Our Company Account Information}";
 		} else {
-
+			log.debug("getOurAccountData() ");
 			return "{Our Company Account Information}";
 		}
 	}
